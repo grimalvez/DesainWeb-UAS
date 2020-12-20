558185a96fb8a35ed63a1f2640d40e93959956f1
cherryoyster <71326912+cherryoyster@users.noreply.github.com>
2020-12-20 18:23:03 +0700

    Revert "update"
    This reverts commit b15c62785a338b9e66bfe1b5e38ed67264fe0d1c.

--- script.js
+++ script.js
@@ -18,5 +18,5 @@ navSlide();
 function Price(){
 		var w=document.getElementById('w').value;
 		var price=w*10000;
-		document.getElementById("result").innerHTML="Estimated price IDR " + price ;
+		document.getElementById("result").innerHTML="Estimated price " + price +" IDR";
 		}
